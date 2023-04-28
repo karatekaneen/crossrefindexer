86ced9f86e08f30874ceeafabef6670da35f4f95
Robin <robin.bauhn@minso.se>
2023-04-28 11:33:30 +0200

    fix: cleanup

--- elastic/elastic.go
+++ elastic/elastic.go
@@ -3,42 +3,50 @@ package elastic
 import (
 	"context"
 	"fmt"
+
 	"github.com/karatekaneen/crossrefindexer"
 )
 
 // Indexer ...
 type Indexer struct {
-	dataToIndex []crossrefindexer.SimplifiedPublication
+	BatchSize int
 }
 
 // Index ...
-func (i *Indexer) Index(ctx context.Context, data chan crossrefindexer.SimplifiedPublication) error {
-	i.dataToIndex = make([]crossrefindexer.SimplifiedPublication, 0, 1000)
+func (i *Indexer) Index(
+	ctx context.Context,
+	data chan crossrefindexer.SimplifiedPublication,
+) error {
+	dataToIndex := make([]crossrefindexer.SimplifiedPublication, 0, i.BatchSize)
 	for {
 		// we receive a publication
 		spub, open := <-data
 		if !open {
 			// If the channel is closed  - We "commit" the publications already in the slice before returning
-			fmt.Println(i.dataToIndex)
+			if err := indexPublications(ctx, dataToIndex); err != nil {
+				return fmt.Errorf("Indexing failed: %w", err)
+			}
 			break
 		}
 
 		// we check if we have room in the slice
-		if len(i.dataToIndex) < cap(i.dataToIndex) {
+		if len(dataToIndex) < cap(dataToIndex) {
 			// - If we do, then append
 			// - fill up the slice if it's not full
-			i.dataToIndex = append(i.dataToIndex, spub)
+			dataToIndex = append(dataToIndex, spub)
 		}
 
-		if len(i.dataToIndex) == cap(i.dataToIndex) {
+		if len(dataToIndex) == cap(dataToIndex) {
 			// - If we don't "commit" the slice to elasticsearch
-			fmt.Println(i.dataToIndex)
-			i.dataToIndex = i.dataToIndex[:0]
+			if err := indexPublications(ctx, dataToIndex); err != nil {
+				return fmt.Errorf("Indexing failed: %w", err)
+			}
+			dataToIndex = dataToIndex[:0]
 		}
 
 	}
 
-	return fmt.Errorf("not implemented")
+	return nil
 }
 
 func indexPublications(ctx context.Context, pubs []crossrefindexer.SimplifiedPublication) error {
